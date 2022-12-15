20f9422c88c0d9935a238d7b83e9d9a63809f5db
follow <follow12587@gmail.com>
2022-12-15 17:25:45 +1000

    new menu view

---
+++ SquardleUI/Views/Menu/MenuView.swift
@@ -0,0 +1,60 @@
+//
+//  MenuView.swift
+//  SquardleUI
+//
+//  Created by Сергей Дубовой on 15.12.2022.
+//
+
+import SwiftUI
+
+struct MenuView: View {
+    @EnvironmentObject var stats: StatsModel
+    var hasGameSave: Bool {
+        stats.hasActiveGame && FileManager.saveExists()
+    }
+    @Binding var option: ContentView.MenuOptions
+    
+    var body: some View {
+        ZStack {
+            ThemeModel.main.backgroundColor
+            
+            VStack (spacing: 20){
+                if stats.hasActiveGame, FileManager.saveExists() {
+                    MenuButton(title: "Продолжить") {
+                        changeOption(.continueGame)
+                    }
+                }
+                
+                MenuButton(title: "Новая Игра"){
+                    changeOption(.newGame)
+                }
+                
+                MenuButton(title: "Как играть?"){
+                    changeOption(.rules)
+                }
+                
+                MenuButton(title: "Статистика"){
+                    changeOption(.stats)
+                }
+                
+            }
+        }
+        .tint(.black)
+        .ignoresSafeArea()
+        .environmentObject(stats)
+    }
+    
+    func changeOption(_ newOption: ContentView.MenuOptions) {
+        withAnimation{
+            option = newOption
+        }
+        playSound("key_clicked")
+    }
+}
+
+struct MenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuView(option: .constant(.none))
+            .environmentObject(StatsModel())
+    }
+}
